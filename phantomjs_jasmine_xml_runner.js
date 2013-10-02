af496e6baa337dcd5b95d9f9d92f9f24de229591
emma lopez <emma.lopez@terminalfour.com>
2013-10-02 14:21:08 +0100

    first commit

---
+++ phantomjs_jasmine_xml_runner.js
@@ -0,0 +1,95 @@
+var htmlrunner,
+resultdir,
+page,
+fs;
+
+
+if ( phantom.args.length !== 2 ) {
+    console.log("Usage: phantom_test_runner.js HTML_RUNNER RESULT_DIR");
+    phantom.exit();
+} else {
+    htmlrunner = phantom.args[0];
+    resultdir = phantom.args[1];
+    page = require("webpage").create();
+    fs = require("fs");
+    
+    // Echo the output of the tests to the Standard Output
+    page.onConsoleMessage = function(msg, source, linenumber) {
+        console.log(msg);
+    };
+
+    page.open(htmlrunner, function(status) {
+
+        if (status === "success") {
+            waitFor(function() { // wait for this to be true
+                return page.evaluate(function() {
+                    return  typeof(jasmine) !== "undefined" && typeof(jasmine.phantomjsXMLReporterPassed) !== "undefined";
+                });
+            }, function() { // once done...
+                // Retrieve the result of the tests
+                var f = null, i, len;
+                suitesResults = page.evaluate(function(){
+                    return jasmine.phantomjsXMLReporterResults;
+                });
+                
+
+                // Save the result of the tests in files
+                for ( i = 0, len = suitesResults.length; i < len; ++i ) {
+                    try {
+                        f = fs.open(resultdir + '/' + suitesResults[i]["xmlfilename"], "w");
+                        f.write(suitesResults[i]["xmlbody"]);
+                        f.close();
+                    } catch (e) {
+                        console.log(e);
+                        console.log("phantomjs> Unable to save result of Suite '"+ suitesResults[i]["xmlfilename"] +"'");
+                    }
+                }
+                
+                // Return the correct exit status. '0' only if all the tests passed
+                phantom.exit(page.evaluate(function(){
+                    return jasmine.phantomjsXMLReporterPassed ? 0 : 1; //< exit(0) is success, exit(1) is failure
+                }));
+            });
+} else {
+    console.log("phantomjs> Could not load '" + htmlrunner + "'.");
+    phantom.exit(1);
+}
+});
+}
+
+
+
+/**
+ * Wait until the test condition is true or a timeout occurs. Useful for waiting
+ * on a server response or for a ui change (fadeIn, etc.) to occur.
+ *
+ * @param testFx javascript condition that evaluates to a boolean,
+ * it can be passed in as a string (e.g.: "1 == 1" or "$('#bar').is(':visible')" or
+ * as a callback function.
+ * @param onReady what to do when testFx condition is fulfilled,
+ * it can be passed in as a string (e.g.: "1 == 1" or "$('#bar').is(':visible')" or
+ * as a callback function.
+ * @param timeOutMillis the max amount of time to wait. If not specified, 3 sec is used.
+ */
+ var waitFor = function(testFx, onReady, timeOutMillis) {
+    var maxtimeOutMillis = timeOutMillis ? timeOutMillis : 30001, //< Default Max Timeout is 3s
+    start = new Date().getTime(),
+    condition = false,
+    interval = setInterval(function() {
+        if ( (new Date().getTime() - start < maxtimeOutMillis) && !condition ) {
+                // If not time-out yet and condition not yet fulfilled
+                condition = (typeof(testFx) === "string" ? eval(testFx) : testFx()); //< defensive code
+            } else {
+                if(!condition) {
+                    // If condition still not fulfilled (timeout but condition is 'false')
+                    console.log("'waitFor()' timeout");
+                    phantom.exit(1);
+                } else {
+                    // Condition fulfilled (timeout and/or condition is 'true')
+                    console.log("'waitFor()' finished in " + (new Date().getTime() - start) + "ms.");
+                    typeof(onReady) === "string" ? eval(onReady) : onReady(); //< Do what it's supposed to do once the condition is fulfilled
+                    clearInterval(interval); //< Stop this interval
+                }
+            }
+        }, 1000); //< repeat check every 100ms
+};
